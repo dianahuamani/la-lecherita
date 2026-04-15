f311d83260f9c558133f401d37bfde48a79d3f52
dianahuamani <diana.huamani@vallegrande.edu.pe>
2026-04-14 21:47:15 -0500

    actualizado

---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuBtn = document.getElementById("menuBtn");
+    const mobileMenu = document.getElementById("mobileMenu");
+
+    menuBtn.addEventListener("click", function () {
+        mobileMenu.classList.toggle("hidden");
+    });
+});
